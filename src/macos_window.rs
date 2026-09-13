f541ca58534add3925dcea797661b7cd0b220ad1
Marcel Koopman <marcel.koopman@gmail.com>
2026-09-13 14:06:08 +0200

    fix: HUDWindow material and unused NSControl import

--- src/macos_window.rs
+++ src/macos_window.rs
@@ -6,8 +6,8 @@ use objc2::rc::{Allocated, Retained};
 use objc2::runtime::{AnyObject, NSObject};
 use objc2::{AnyThread, MainThreadMarker, MainThreadOnly, define_class, msg_send, sel};
 use objc2_app_kit::{
-    NSApplication, NSAutoresizingMaskOptions, NSBackingStoreType, NSButton, NSColor, NSControl,
-    NSFont, NSFontAttributeName, NSForegroundColorAttributeName, NSScrollView, NSTextView,
+    NSApplication, NSAutoresizingMaskOptions, NSBackingStoreType, NSButton, NSColor, NSFont,
+    NSFontAttributeName, NSForegroundColorAttributeName, NSScrollView, NSTextView,
     NSVisualEffectBlendingMode, NSVisualEffectMaterial, NSVisualEffectState, NSVisualEffectView,
     NSWindow, NSWindowStyleMask, NSWindowTitleVisibility,
 };
@@ -101,7 +101,7 @@ pub fn show(formatted: &str, kind: FormatKind) -> Result<(), String> {
     let bounds = content.bounds();
 
     let frosted = NSVisualEffectView::initWithFrame(NSVisualEffectView::alloc(mtm), bounds);
-    frosted.setMaterial(NSVisualEffectMaterial::HudWindow);
+    frosted.setMaterial(NSVisualEffectMaterial::HUDWindow);
     frosted.setBlendingMode(NSVisualEffectBlendingMode::BehindWindow);
     frosted.setState(NSVisualEffectState::Active);
     frosted.setAutoresizingMask(
