1b341b8fd4bce1676205d0f2bf080bda8afe689a
Marcel Koopman <marcel.koopman@gmail.com>
2026-09-16 20:50:46 +0200

    import NSAnimationContext for preview fade

--- src/macos_window.rs
+++ src/macos_window.rs
@@ -6,9 +6,9 @@ use objc2::rc::{Allocated, Retained};
 use objc2::runtime::{AnyObject, NSObject};
 use objc2::{AnyThread, MainThreadMarker, MainThreadOnly, define_class, msg_send, sel};
 use objc2_app_kit::{
-    NSApplication, NSAutoresizingMaskOptions, NSBackingStoreType, NSButton, NSColor, NSFont,
-    NSFontAttributeName, NSForegroundColorAttributeName, NSModalResponseOK, NSSavePanel,
-    NSScrollView, NSTextView, NSVisualEffectBlendingMode, NSVisualEffectMaterial,
+    NSAnimationContext, NSApplication, NSAutoresizingMaskOptions, NSBackingStoreType, NSButton,
+    NSColor, NSFont, NSFontAttributeName, NSForegroundColorAttributeName, NSModalResponseOK,
+    NSSavePanel, NSScrollView, NSTextView, NSVisualEffectBlendingMode, NSVisualEffectMaterial,
     NSVisualEffectState, NSVisualEffectView, NSWindow, NSWindowStyleMask, NSWindowTitleVisibility,
 };
 use objc2_foundation::{
