9cca082e023c1e05d7e83bd559a8fd6a009a0d48
Marcel Koopman <marcel.koopman@gmail.com>
2026-09-16 21:35:05 +0200

    pack Compress into the context toolbar

--- src/macos_window_ops.rs
+++ src/macos_window_ops.rs
@@ -61,8 +61,10 @@ fn apply_toolbar_for_kind(kind: FormatKind) {
     let source = SOURCE_TEXT.with(|slot| slot.borrow().clone());
     let show_redact = toolbar_visibility::shows_redact(kind);
     let show_df = toolbar_visibility::shows_dataframe(kind) || dataframe::try_format(&source).is_some();
+    let show_compress = toolbar_visibility::shows_compress(kind);
     set_button_hidden(&REDACT_BUTTON, !show_redact);
     set_button_hidden(&DATAFRAME_BUTTON, !show_df);
+    set_button_hidden(&COMPRESS_BUTTON, !show_compress);
 
     let height = WINDOW.with(|slot| {
         slot.borrow()
@@ -76,6 +78,10 @@ fn apply_toolbar_for_kind(kind: FormatKind) {
     x += TOOLBAR_BTN_W + TOOLBAR_GAP;
     place_button(&FORMAT_BUTTON, x, y);
     x += TOOLBAR_BTN_W + TOOLBAR_GAP;
+    if show_compress {
+        place_button(&COMPRESS_BUTTON, x, y);
+        x += TOOLBAR_BTN_W + TOOLBAR_GAP;
+    }
     if show_redact {
         place_button(&REDACT_BUTTON, x, y);
         x += TOOLBAR_BTN_W + TOOLBAR_GAP;
