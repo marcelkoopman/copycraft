eedbce730367ae7b99debd8fe39f5342ab7e7186
Marcel Koopman <marcel.koopman@gmail.com>
2026-09-16 21:34:46 +0200

    import compress module

--- src/macos_window.rs
+++ src/macos_window.rs
@@ -17,6 +17,7 @@ use objc2_foundation::{
 };
 
 use crate::clipboard;
+use crate::compress;
 use crate::dataframe;
 use crate::format::{self, FormatKind};
 use crate::macos_preview_text::{configure_scrolling_text, editor_font, set_body};
