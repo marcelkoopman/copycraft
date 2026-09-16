9467ef6c4a9b869babcfc5ef5f111238dccc4832
Marcel Koopman <marcel.koopman@gmail.com>
2026-09-16 20:54:42 +0200

    import NSAnimatablePropertyContainer for scroll.animator()

--- src/macos_window.rs
+++ src/macos_window.rs
@@ -6,10 +6,11 @@ use objc2::rc::{Allocated, Retained};
 use objc2::runtime::{AnyObject, NSObject};
 use objc2::{AnyThread, MainThreadMarker, MainThreadOnly, define_class, msg_send, sel};
 use objc2_app_kit::{
-    NSAnimationContext, NSApplication, NSAutoresizingMaskOptions, NSBackingStoreType, NSButton,
-    NSColor, NSFont, NSFontAttributeName, NSForegroundColorAttributeName, NSModalResponseOK,
-    NSSavePanel, NSScrollView, NSTextView, NSVisualEffectBlendingMode, NSVisualEffectMaterial,
-    NSVisualEffectState, NSVisualEffectView, NSWindow, NSWindowStyleMask, NSWindowTitleVisibility,
+    NSAnimatablePropertyContainer, NSAnimationContext, NSApplication, NSAutoresizingMaskOptions,
+    NSBackingStoreType, NSButton, NSColor, NSFont, NSFontAttributeName,
+    NSForegroundColorAttributeName, NSModalResponseOK, NSSavePanel, NSScrollView, NSTextView,
+    NSVisualEffectBlendingMode, NSVisualEffectMaterial, NSVisualEffectState, NSVisualEffectView,
+    NSWindow, NSWindowStyleMask, NSWindowTitleVisibility,
 };
 use objc2_foundation::{
     NSArray, NSMutableAttributedString, NSPoint, NSRange, NSRect, NSSize, NSString,
