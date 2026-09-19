2a037f3a534e2b3b9e15c9114ebec886f050fd52
marcelkoopman <marcel.koopman@gmail.com>
2026-09-19 18:59:56 +0200

    show Copycraft version in the menubar menu
    Add a disabled secondary item above Quit so the running package
    version is visible without opening About.

--- src/menubar.rs
+++ src/menubar.rs
@@ -275,6 +275,7 @@ impl App {
             None,
         ));
         let _ = menu.append(&PredefinedMenuItem::separator());
+        let _ = menu.append(&version_item());
         let _ = menu.append(&IconMenuItem::with_id_and_native_icon(
             "quit",
             "Quit",
@@ -286,6 +287,17 @@ impl App {
         let _ = self.tray.set_tooltip(Some("Copycraft"));
         self.tray.set_title(None::<&str>);
     }
+}
+
+fn version_label() -> String {
+    format!("Copycraft {}", env!("CARGO_PKG_VERSION"))
+}
+
+fn version_item() -> MenuItem {
+    let label = version_label();
+    let item = MenuItem::new(&label, false, None);
+    item.set_styled_text(vec![(label, TextStyle::Secondary)]);
+    item
 }
 
 fn appearance_menu() -> Submenu {
@@ -383,4 +395,17 @@ pub fn run() -> Result<(), Box<dyn std::error::Error>> {
     let event_loop = EventLoop::new()?;
     event_loop.run_app(&mut app)?;
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::version_label;
+
+    #[test]
+    fn version_label_includes_package_version() {
+        assert_eq!(
+            version_label(),
+            format!("Copycraft {}", env!("CARGO_PKG_VERSION"))
+        );
+    }
 }
